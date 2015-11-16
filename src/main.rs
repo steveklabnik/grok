4cdff5a2058f0be852d8550739f6361299bd59f4
Steve Klabnik <steve@steveklabnik.com>
2015-11-15 23:55:01 -0500

    Add 'grok' to just run
    and then print the final state

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::io::BufReader;
+use std::fs::File;
+
+extern crate grok;
+
+use grok::Cpu;
+
+fn main() {
+    let filename = env::args().skip(1).next().unwrap_or(String::from("sample.asm"));
+
+    let f = File::open(filename).expect("Couldn't find file");
+    let reader = BufReader::new(f);
+
+    let mut cpu = Cpu::new();
+    cpu.load(reader);
+
+    cpu.run();
+
+    println!("Final state:\n{}", cpu);
+}
